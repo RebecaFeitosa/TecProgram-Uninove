521fe8c832310ab8ac169627afeab96080d355d2
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-09-01 21:41:12 -0300

    exercicio

--- Aula03/Program.cs
+++ Aula03/Program.cs
@@ -32,7 +32,15 @@ static void p1002(){
 }
 
 static void p1003(){
-    Console.Write("Prog 3");
+    /* Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule
+    a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.  */
+
+    int a, b, soma;
+    a = Convert.ToInt32(Console.ReadLine());
+    b = Convert.ToInt32(Console.ReadLine());
+    soma = a + b;
+
+    Console.Write("SOMA = {0}\n", soma);
 }
 
     int prog;
