cb51bfbc6880dee0c59668547d069c685241e401
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-09-22 21:35:49 -0300

    att

--- Aula01/Program.cs
+++ Aula01/Program.cs
@@ -1,18 +1,22 @@
-﻿/* Variável = espaço reservado na memória do computador.
-int = inteiro
-double ou decimal = real
-string = caracteres 
-true(1) ou false (0)
-*/
+﻿// Variável = espaço reservado na memória do computador.
 
-int a;
+// int = inteiro
+short a = 0215;
+
+// double ou decimal = real
+double salario = 1000.50;
+
+// string = caracteres 
+string nome = "Rebeca";
+
+// true(1) ou false (0)
+bool aula = true;
 
 // Para escrever na tela = Console.Write("String");
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 // Para ler um valor = Console.ReadLine();
-Console.WriteLine("Digite o valor de a: ");
-a = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("O valor digitado é= ");
-Console.Write(a);
+Console.WriteLine("a = " + a);
+Console.WriteLine("Salario = " + salario);
+Console.WriteLine("Nome = " + nome);
+Console.WriteLine("Estamos em aula? " + aula);
