45507bd7b02b85d38c5b17c544281ce88d184b77
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-10-06 20:20:38 -0300

    condições

--- Aula05_condicoes/if_simples-composto/Program.cs
+++ Aula05_condicoes/if_simples-composto/Program.cs
@@ -1,15 +1,35 @@
-﻿int n = -100;
+﻿// Exemplo de condições
+
+int n = -100;
+
 
 //Decisão SIMPLES
 if (n > 0){
-    Console.Write("POSITIVO!");
+    //  VERDADEIRO
+    Console.Write("POSITIVO!\n");
 }
+
 
 //Decisão COMPOSTA
 if (n > 0){
-    //VERDADEIRO
-    Console.Write("POSITIVO!");
+    //  VERDADEIRO
+    Console.Write("POSITIVO!\n");
 }else{
-    //FALSO
-    Console.Write("NEGATIVO!");
+    //  FALSO
+    Console.Write("NEGATIVO!\n");
+}
+
+
+//Decisão ANINHADA/ENCADEADA (Multiplas perguntas)
+if(n > 0){           // 1ª Pergunta
+     // VERDADEIRO
+    Console.Write("POSITIVO!\n");
+}else{              
+    if(n < 0){      // 2ª Pergunta
+    //  VERDADEIRO
+    Console.Write("NEGATIVO!\n");
+    }else{
+        //FALSO
+        Console.Write("NULO!\n");
+    }
 }
