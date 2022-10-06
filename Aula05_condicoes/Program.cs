37be6119e0c40b8d336b2f897f363f6b00816c01
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2022-10-06 20:10:12 -0300

    condições

--- Aula05_condicoes/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿int n = -10;
-
-//Decisão simples
-if (n > 0){
-    Console.Write("POSITIVO!");
-}else{
-    Console.Write("NEGATIVO!");
-}
---
+++ Aula05_condicoes/if_simples-composto/Program.cs
@@ -0,0 +1,15 @@
+﻿int n = -100;
+
+//Decisão SIMPLES
+if (n > 0){
+    Console.Write("POSITIVO!");
+}
+
+//Decisão COMPOSTA
+if (n > 0){
+    //VERDADEIRO
+    Console.Write("POSITIVO!");
+}else{
+    //FALSO
+    Console.Write("NEGATIVO!");
+}
